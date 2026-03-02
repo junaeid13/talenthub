f6bcbdbdb211a735a5acd57c618bd74230dc7219
ba065869 <junaeid.ahmed@stud.uni-bamberg.de>
2026-03-02 23:11:00 +0100

    Initial commit

---
+++ src/main/java/org/junaeid/talenthub/TalenthubApplication.java
@@ -0,0 +1,13 @@
+package org.junaeid.talenthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalenthubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalenthubApplication.class, args);
+    }
+
+}
